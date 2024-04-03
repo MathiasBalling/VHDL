f87eed3d4003b8eb3a7fb769452d9eebbe030e1a
Mathias Balling <math260r@gmail.com>
2024-04-03 19:12:05 +0200

    Lab 5

---
+++ Adder/full_adder.vhdl
@@ -0,0 +1,39 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+ 
+entity full_adder is
+  port (
+  i_A : in std_logic;
+  i_B  : in std_logic;
+  i_C : in std_logic;
+
+  o_S : out std_logic;
+  o_C : out std_logic
+    );
+end full_adder;
+ 
+architecture rtl of full_adder is
+  component half_adder
+    port (
+    i_A : in std_logic;
+    i_B  : in std_logic;
+
+    o_S : out std_logic;
+    o_C : out std_logic
+  );
+  end component;
+
+  -- internal signals(wires)
+  signal carry1 : std_logic;
+  signal carry2 : std_logic;
+  signal sum1 : std_logic;
+begin
+  ha1 : half_adder 
+    port map (i_A => i_A, i_B => i_B, o_S => sum1, o_C => carry1);
+
+  ha2 : half_adder 
+    port map (i_A => sum1, i_B => i_C, o_S => o_S, o_C => carry2);
+
+  o_C <= carry1 or carry2;
+end rtl;
---
+++ Adder/full_adder_tb.vhdl
@@ -0,0 +1,43 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use std.env.finish;
+ 
+entity full_adder_tb is
+end full_adder_tb;
+ 
+architecture behave of full_adder_tb is
+  signal i_A, i_B, i_C : std_logic;
+  signal o_S, o_C : std_logic;
+
+  component full_adder
+    port (
+      i_A, i_B, i_C : in std_logic;
+      o_S, o_C : out std_logic
+    );
+  end component;
+begin
+  UUT : entity work.full_adder
+    port map (
+      i_A => i_A,
+      i_B => i_B,
+      i_C => i_C,
+      o_S => o_S,
+      o_C => o_C
+    );
+
+  process is
+  begin
+    i_A <= '0'; i_B <= '0'; i_C <= '0'; wait for 10 ns;
+    i_A <= '0'; i_B <= '0'; i_C <= '1'; wait for 10 ns;
+    i_A <= '0'; i_B <= '1'; i_C <= '0'; wait for 10 ns;
+    i_A <= '0'; i_B <= '1'; i_C <= '1'; wait for 10 ns;
+    i_A <= '1'; i_B <= '0'; i_C <= '0'; wait for 10 ns;
+    i_A <= '1'; i_B <= '0'; i_C <= '1'; wait for 10 ns;
+    i_A <= '1'; i_B <= '1'; i_C <= '0'; wait for 10 ns;
+    i_A <= '1'; i_B <= '1'; i_C <= '1'; wait for 10 ns;
+    wait for 10 ns;
+
+    finish;
+  end process;
+end behave;
